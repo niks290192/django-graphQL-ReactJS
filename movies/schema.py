f2d23da7b35a7fa4c03340acd3d43b92bb9c2cc2
Nikhil Arora <nikhil290192@gmail.com>
2021-09-29 18:32:24 +0530

    Progress

---
+++ movies/schema.py
@@ -0,0 +1,13 @@
+import graphene
+import movies.api.schema
+
+
+class Query(movies.api.schema.Query, graphene.ObjectType):
+    pass
+
+
+class Mutation(movies.api.schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
